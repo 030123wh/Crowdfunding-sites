f9ebc83056638e961a10894c805427c2c537c226
030123wh <145890361+030123wh@users.noreply.github.com>
2024-10-02 21:59:40 +0800

    1

--- api/crowdfunding_db.js
+++ api/crowdfunding_db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'root123456',
+    password: '1111',
     database: 'crowdfunding_db'
 });
 
